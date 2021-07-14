47f6de0673f4c3b00e0a88672c777e0d85465793
Nara <narawit.tub@outlook.co.th>
2021-07-14 21:27:38 +0700

    {Client-API} chore: enable CORS

--- API/Startup.cs
+++ API/Startup.cs
@@ -37,6 +37,13 @@ namespace API
             {
                 option.UseSqlite(_config.GetConnectionString("DefaultConnection"));
             });
+            services.AddCors(option =>
+            {
+                option.AddPolicy("CorsPolicy", policy =>
+                {
+                    policy.AllowAnyMethod().AllowAnyHeader().WithOrigins("http://localhost:3000");
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -52,6 +59,8 @@ namespace API
             // app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseCors("CorsPolicy");
 
             app.UseAuthorization();
 
--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -1,17 +1,17 @@
-import React, {useState, useEffect} from 'react';
-import logo from './logo.svg';
-import './App.css';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import axios from "axios";
 
 function App() {
-  const [activities, setActivities] = useState([])
+  const [activities, setActivities] = useState([]);
 
   useEffect(() => {
-    axios.get("http://localhost:5000/api/activities").then(response => {
+    axios.get("http://localhost:5000/api/activities").then((response) => {
       console.log(response);
       setActivities(response.data);
-    })
-  }, [])
+    });
+  }, []);
 
   return (
     <div className="App">
