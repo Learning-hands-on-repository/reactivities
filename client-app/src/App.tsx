8a48f33922716099608f5d4885309028e9f9b635
Nara <narawit.tub@outlook.co.th>
2021-07-15 09:16:42 +0700

    {App} :lipstick: style: add semantic ui as a css framework

--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -1,28 +1,25 @@
 import React, { useState, useEffect } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import axios from "axios";
+import { Header, List } from "semantic-ui-react";
 
 function App() {
   const [activities, setActivities] = useState([]);
 
   useEffect(() => {
     axios.get("http://localhost:5000/api/activities").then((response) => {
-      console.log(response);
       setActivities(response.data);
     });
   }, []);
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <ul>
+      <Header as="h2" icon="users" content="Some header" />
+        <List>
           {activities.map((activity: any) => (
-            <li key={activity.id}>{activity.title}</li>
+            <List.Item key={activity.id}>{activity.title}</List.Item>
           ))}
-        </ul>
-      </header>
+        </List>
     </div>
   );
 }
